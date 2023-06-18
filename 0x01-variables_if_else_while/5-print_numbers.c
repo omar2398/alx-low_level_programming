a2cb7986c65ff1a1b751fcd0da1642549b8c7ae7
OmAR <omarsteven2398@gmail.com>
2023-06-19 01:45:02 +0300

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%i", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
