0b2500f8f1b32ab3ef9605f61862693a082b35e8
OmAR <omarsteven2398@gmail.com>
2023-06-19 01:20:57 +0300

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,8 +11,13 @@
 
 int main(void)
 {
-	for (int i = 97 ; i <= 122 ; i++)
-		putchar(i);
-	printf("\n");
+	char ch = 'a'
+
+	while (ch <= z)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
 	return (0);
 }
