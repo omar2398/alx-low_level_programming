04ba1099525a2a954051e3cce637eb356ce2ee23
OmAR <omarsteven2398@gmail.com>
2023-06-19 01:13:17 +0300

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 97 ; i <= 122 ; i++)
+		putchar(i);
+	printf("\n";)
+	return (0);
+}
