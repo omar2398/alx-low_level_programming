a9590a51e28b67757be336c37a82aa0cfa9abdbd
OmAR <omarsteven2398@gmail.com>
2023-06-24 05:15:07 +0300

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _isalpha(int c); - show if the alphapt is lowercase or not .
+ * _isalpha - show if the alphapt is lowercase or not .
  * @c : check the input
  * Return: int
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 97))
 		return (1);
