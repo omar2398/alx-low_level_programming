e7ac92a8656178cc634c79a9c62a6397492afb33
OmAR <omarsteven2398@gmail.com>
2023-06-24 04:28:14 +0300

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	return (0);
+}
