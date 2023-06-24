972b5b26e8293b19c0edde478477c9172471b49b
OmAR <omarsteven2398@gmail.com>
2023-06-24 05:44:28 +0300

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - show the sign of numbers  .
+ * @n : check the input
+ * Return: int
+ */
+int int _abs(int n)
+{
+	if (n >= 0)
+	{
+	putchar(n);
+	}
+	else
+	{
+	n = -n;
+	putchar(n);
+	}
+}
