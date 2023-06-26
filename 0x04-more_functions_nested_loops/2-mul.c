09e9f3b366357f7799f96e62535a233af4590ebf
OmAR <omarsteven2398@gmail.com>
2023-06-26 23:13:50 +0300

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - is an internal function
+ * Description: 'the program's description'
+ * @a : input1
+ * @b : input2
+ * Return: Always 0 (Success)
+ */
+
+int int mul(int a, int b)
+{
+	return (a * b);
+}
