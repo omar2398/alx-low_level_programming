01498e5ba212da8ac4eea7c5b47868c4187326b5
OmAR <omarsteven2398@gmail.com>
2023-06-26 23:29:53 +0300

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - is an internal function
+ * _putchar - terturn function
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+		_putchar(i);
+
+}
