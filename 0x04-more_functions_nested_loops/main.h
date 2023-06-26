6f2684ad882317345f0f06fbffb79ca049387e44
OmAR <omarsteven2398@gmail.com>
2023-06-26 23:49:41 +0300

    a

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,18 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <unistd.h>
 
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers();
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif
