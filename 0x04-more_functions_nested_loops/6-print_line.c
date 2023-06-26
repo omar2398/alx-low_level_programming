ac262c01437d64240c9d6e61dbf7c582f4c21c2e
Omar mohamed <118669490+omar2398@users.noreply.github.com>
2023-06-27 00:07:53 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * print_line - print the numbers from 0 to 9
- * followed by a new line.
- * @n: input
- * Return: Void.
+ * print_line - print a line of _ of length n.
+ * @n: the length of line.
+ *
+ * Return: Void
  */
 void print_line(int n)
 {
-	for (int i = 0; i <= n; i++)
+	for (; n > 0; n--)
+	{
 		_putchar('_');
+	}
 	_putchar('\n');
 }
