ccc0edc08cd75f146de058874ced039e5c92320f
OmAR <omarsteven2398@gmail.com>
2023-06-26 23:10:55 +0300

    a

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * _isupper - is an internal function
+ * _isdigit - is an internal function
  * Description: 'the program's description'
  * @c : input
  *
  * Return: Always 0 (Success)
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
