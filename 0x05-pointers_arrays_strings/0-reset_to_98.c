5c76bd0c8bde78cf8ed28567445eb0dd96a2f279
OmAR <omarsteven2398@gmail.com>
2023-06-28 03:21:51 +0300

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: 'the program's description'
+ * @n : input parameters
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
