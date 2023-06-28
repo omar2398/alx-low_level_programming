c96e24b03dcc5fd83885872775278977dd970daf
OmAR <omarsteven2398@gmail.com>
2023-06-28 03:58:10 +0300

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Entry point
+ *
+ * Description: 'the program's description'
+ * @s : input parameters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	strlen(s);
+}
