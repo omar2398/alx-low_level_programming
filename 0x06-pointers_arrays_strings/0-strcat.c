db2b0620e547fa1db6fc74247fd9be3b98111d10
OmAR <omarsteven2398@gmail.com>
2023-07-01 01:28:27 +0300

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strcat - Entry point
+ * @dest : input
+ * @src : input 2
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
