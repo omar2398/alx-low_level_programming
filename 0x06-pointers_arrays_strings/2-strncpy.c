b9a2342cabc01e8fe74bec6224d3869a935176bf
OmAR <omarsteven2398@gmail.com>
2023-07-01 02:33:25 +0300

    a

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
 /**
- * _strncpy - concatenates n bytes from a string to another
+ * _strncpy - copies a string
  * @dest: destination string
  * @src: source string
- * @n: number of bytes of str to concatenate
+ * @n: number of bytes to copy
  *
- * Return: a pointer to the resulting string dest
+ * Return: pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
 		dest[i] = src[i];
-	if (i < n)
+		i++;
+	}
+
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
+
 	return (dest);
 }
