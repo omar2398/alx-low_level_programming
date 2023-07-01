ec6271d76bd7e436909e668e84274b73f2cc58fb
OmAR <omarsteven2398@gmail.com>
2023-07-01 03:07:19 +0300

    a

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - compares two strings
+ * @a: first string to compare
+ * @n: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+void reverse_array(int *a, int n)
+{
+	int b[n], j = 0;
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		b[j] = a[i];
+		j++;
+	}
+	return (b);
+}
