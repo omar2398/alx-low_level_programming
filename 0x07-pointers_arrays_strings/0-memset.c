e66bc9d77f54b2d63cf14d3cfda3954659626ac7
OmAR <omarsteven2398@gmail.com>
2023-07-04 02:45:00 +0300

    a

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memset - Entry point
+ * @s : input parameters
+ * @b : input
+ * @n : input
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
