0b4f4acfbbf969ee97d171f4230b9ff1a2a6c304
OmAR <omarsteven2398@gmail.com>
2023-07-07 02:44:14 +0300

    a

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - Entry point
+ * @s : input parametar
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
