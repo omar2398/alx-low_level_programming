d67ac4edb67dc9b536972e8f58c617f6e3181692
OmAR <omarsteven2398@gmail.com>
2023-07-07 03:15:50 +0300

    a

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,16 +5,11 @@
  *
  * Return: Void.
  */
-int i = 0;
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		i++;
-		_strlen_recursion(s + 1);
+		return (0);
 	}
-	else
-	{
-	return (i);
-	}
+	return (1 + _strlen_recursion(s + 1));
 }
