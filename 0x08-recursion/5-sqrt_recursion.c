7366d69218a91fe161ec4ccde1f9878db25337b5
OmAR <omarsteven2398@gmail.com>
2023-07-07 03:31:00 +0300

    a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,17 +5,23 @@
  *
  * Return: the natural square of n, -1 if there is no natural roots.
  */
+
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _sqrt_recursion(int n)
 {
-	int i = 1;
-
-	while (i * i <= n)
-	{
-		if (i == n)
-		{
-			return (i);
-		}
-		i++;
-	}
-	return (-1);
+	return (sqrt2(n, 1));
 }
