5249dc6cea7ae02a1766ca0e6c86974b81d36fc7
OmAR <omarsteven2398@gmail.com>
2023-07-10 04:31:12 +0300

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @argc : input counter
+ * @argv : input String
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	return (0);
+}
