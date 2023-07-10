de6ecc84cbc09f51bf3a5cf8f79884a14d0ab5db
OmAR <omarsteven2398@gmail.com>
2023-07-10 04:36:28 +0300

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @argc : input counter
+ * @argv : input String
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
