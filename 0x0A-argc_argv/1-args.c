8d9a0f206ab3eee61a1a5b4f6359f9a02afb9c81
OmAR <omarsteven2398@gmail.com>
2023-07-10 04:40:30 +0300

    a

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,8 +10,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[])
 {
+	(void)argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
