f3478f57ec69edaa19292d1150ee737cf324edba
OmAR <omarsteven2398@gmail.com>
2023-07-11 23:04:07 +0300

    a

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @argc : input counter
+ * @argv : input String
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv[1] != '\0' && argv[2] != '\0')
+	{
+		printf("%d\n", argv[1] * argv[2]);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
