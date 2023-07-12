0f298b574d636f12ecdf0af36eb990aa663a96cb
OmAR <omarsteven2398@gmail.com>
2023-07-13 02:59:03 +0300

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup - Entry point
+ *
+ * Description: 'the program's description'
+ * @str:input
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strdup(char *str);
+{
+	int l;
+	char *ptr;
+	int i;
+
+	l = strlen(str);
+	*ptr = malloc(l);
+
+	if (l == 0 || ptr == 0)
+		return (NULL);
+	for (i = 0; i < l; i++)
+		ptr[i] = str[i];
+	return (ptr);
+}
