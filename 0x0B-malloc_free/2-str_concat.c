930c0de1a408404c000210a31926b447f1620170
OmAR <omarsteven2398@gmail.com>
2023-07-13 03:30:30 +0300

    a

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * str_concat - Entry point
+ *
+ * Description: 'the program's description'
+ * @s1:input
+ * @s2:input
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *str_concat(char *s1, char *s2);
+{
+	int l;
+	int i;
+	int j;
+	int all;
+	char *str;
+
+	all = strlen(s1) + strlen(s2);
+	l = strlen(s1);
+	ptr = (char *)malloc((all + 1) * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < l; i++)
+		ptr[i] = s1[i];
+	for (j = 0; i < all; i++)
+	{
+		ptr[i] = s1[j];
+		i++;
+	}
+
+	return (str);
+}
