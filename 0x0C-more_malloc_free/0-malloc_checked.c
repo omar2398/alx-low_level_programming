fd724b8a2b5ad104bbcfb752e5fa82c7eae2e08e
OmAR <omarsteven2398@gmail.com>
2023-07-17 18:01:19 +0300

    a

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * malloc_checked - Entry point
+ *
+ * Description: 'the program's description'
+ * @b: input
+ *
+ * Return: Always 0 (Success)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *m;
+
+	m = malloc(b);
+	if (m == NULL)
+		exit(98);
+	return (m);
+}
