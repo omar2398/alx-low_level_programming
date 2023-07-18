c7148c9f914229ad68b7054ab41fca480dac9712
OmAR <omarsteven2398@gmail.com>
2023-07-18 04:10:30 +0300

    a

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,29 +3,29 @@
 #include <stdlib.h>
 
 /**
- *  array_range- Entry point
- *@min: size of the array
- *@max: size of the type
+ * array_range - Entry point
+ *@min: number min
+ *@max: number max
  * Return: a pointer to an array
  */
 int *array_range(int min, int max)
 {
-	int i;
-	int *omar = NULL;
+	int *array = NULL;
+	int i, t, count;
 
+	count = 0;
 	if (min > max)
 		return (NULL);
-	omar = malloc(((max - min) + 1) * sizeof(int));
-	if (!omar)
+	t = (max - min) + 1;
+	array = malloc(t * sizeof(int));
+	if (array != NULL)
 	{
-		return (NULL);
+		for (i = min; i <= max; i++)
+		{
+			array[count] = i;
+			count++;
+		}
+		return (array);
 	}
-	else
-	{
-	for (i = min; i <= max; i++)
-	{
-		omar[i] = i;
-	}
-	return (omar);
-	}
+	return (NULL);
 }
