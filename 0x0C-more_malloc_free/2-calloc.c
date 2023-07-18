1296c988d05f4f8613d1fca6f95e73365da50deb
OmAR <omarsteven2398@gmail.com>
2023-07-18 03:56:11 +0300

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,32 +4,27 @@
 
 /**
  * _calloc - Entry point
- * @nmemb: string 1
- * @size: string 2
- * Return: pointer should point to a newly allocated space in memory or NULL
+ *@nmemb: size of the array
+ *@size: size of the type
+ * Return: a pointer to an array
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *omar = NULL;
-	unsigned int i, j;
+	void *array = NULL;
+	unsigned int i;
+	unsigned int t;
 	char *f;
 
-	if (nmemb == 0 || size == 0)
-	{
+	if (size == 0 || nmemb == 0)
 		return (NULL);
-	}
-
-	omar = malloc(nmemb * size);
-	f = (char *)omar;
-	if (omar == NULL)
+	t = nmemb * size;
+	array = malloc(t);
+	f = (char *)array;
+	if (f != NULL)
 	{
-		return (NULL);
-	}
-	else
-	{
-		for (i = 0; i < (nmemb * size); i++)
+		for (i = 0; i < t; i++)
 			f[i] = 0;
 		return (f);
 	}
+	return (NULL);
 }
