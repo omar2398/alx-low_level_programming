b61b763a3b1603ba449b13fa7bf7e56b990059be
OmAR <omarsteven2398@gmail.com>
2023-07-19 02:25:58 +0300

    a

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	prinf("%s\n", __FILE__)
+	return (0);
+}
