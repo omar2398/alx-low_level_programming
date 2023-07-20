1768fb691b61e399da59adfce7701c2a155318d0
OmAR <omarsteven2398@gmail.com>
2023-07-20 17:23:46 +0300

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - Entry point
+ * @d:input
+ * @name:input
+ * @age:input
+ * @owner:input
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
