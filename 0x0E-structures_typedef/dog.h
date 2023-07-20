6853d1dc11e875f70cb2c6475992486d3d9533d0
OmAR <omarsteven2398@gmail.com>
2023-07-20 17:04:56 +0300

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - Entry point
+ * @name:input
+ * @age:input
+ * @owner:input
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
