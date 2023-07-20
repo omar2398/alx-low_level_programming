23bea649d77cbeea3434577045f1ba5bb08650fb
OmAR <omarsteven2398@gmail.com>
2023-07-20 19:28:03 +0300

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,25 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
 
+int _putchar(char c);
 /**
- * struct dog - Entry point
- * @name:input
- * @age:input
- * @owner:input
- * Description: 'the program's description'
- *
- *
- * Return: Always 0 (Success)
- */
+*struct dog - Initialize struct dog
+*@name:name type char
+*@age: age type float
+*@owner: name of owner tyme char
+*
+*Description: name, age and owner of a dog
+*/
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
+} dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-#endif
+typedef dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif 
