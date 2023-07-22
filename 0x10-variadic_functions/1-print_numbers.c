871ad4a830e5c336e8e63312d8c6dae9e7dd13c9
OmAR <omarsteven2398@gmail.com>
2023-07-22 04:24:17 +0300

    a

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Entry point
+ * @n:input
+ * @separator:separator
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list omar;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+	va_start(omar, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1) && separator != NULL)
+			printf("%d%s", va_arg(omar, unsigned int), separator);
+		else
+			printf("%d", va_arg(omar, unsigned int));
+	}
+	printf("\n");
+	va_end(omar);
+
+}
