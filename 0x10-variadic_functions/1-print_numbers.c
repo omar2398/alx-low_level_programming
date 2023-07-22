3e83c31deb33cb1599a84f8e630fda0b2a05fbdd
OmAR <omarsteven2398@gmail.com>
2023-07-22 04:31:55 +0300

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,17 +10,17 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
+	va_list omar;
 	unsigned int i;
 
-	va_start(valist, n);
+	va_start(omar, n);
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1) && separator != NULL)
-			printf("%d%s", va_arg(valist, unsigned int), separator);
+			printf("%d%s", va_arg(omar, unsigned int), separator);
 		else
-			printf("%d", va_arg(valist, unsigned int));
+			printf("%d", va_arg(omar, unsigned int));
 	}
 	printf("\n");
-	va_end(valist);
+	va_end(omar);
 }
