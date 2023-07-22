e0db85a128d14a31a7ac06fce39ce65ff89d64f1
OmAR <omarsteven2398@gmail.com>
2023-07-22 04:06:46 +0300

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -22,7 +22,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(omar, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(omar, int);
+		sum += va_arg(omar, unsigned int);
 	}
 	va_end(omar);
 	return (sum);
