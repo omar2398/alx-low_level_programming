f62b0770b0684d85bcb3eed84014246e35ee96a2
OmAR <omarsteven2398@gmail.com>
2023-08-11 18:49:57 +0300

    omar

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@
  * Return: 1 on success, -1 on failure
  */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int om, mo, counter = 0;
 	char *buffer;
